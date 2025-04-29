bad5ecde96d57d2c7b189c07f79ff3098fdd764c
abhay itkare <abhay@designi4t.com>
2025-04-29 17:45:52 +0530

    modify monopole backend

---
+++ tower/app1/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+class ListOfStructure(models.Model):
+    structure = models.CharField(max_length=100,unique=True)
+
+    def __str__(self):
+        return self.structure
+
+
+from django.db import models
+
+class MonopoleDeadend(models.Model):
+    CIRCUIT_CHOICES = [(i, str(i)) for i in range(1, 11)]
+
+    structure = models.ForeignKey('ListOfStructure', on_delete=models.CASCADE, related_name='monopole_deadends', unique=True)
+
+    num_3_phase_circuits = models.PositiveIntegerField(choices=CIRCUIT_CHOICES)
+    num_shield_wires = models.PositiveIntegerField(choices=CIRCUIT_CHOICES)
+    num_1_phase_circuits = models.PositiveIntegerField(choices=CIRCUIT_CHOICES)
+    num_communication_cables = models.PositiveIntegerField(choices=CIRCUIT_CHOICES)
+
+    def __str__(self):
+        return f"MonopoleDeadend {self.id} - Structure: {self.structure}"
+    
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['structure'], name='unique_monopole_per_structure')
+        ]
+
+
+
+class UploadedFile1(models.Model):
+    structure = models.ForeignKey(ListOfStructure, on_delete=models.CASCADE, related_name='uploaded_files')
+    file = models.FileField(upload_to='uploads/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.file.name} ({self.structure.structure})"
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['structure'], name='unique_file_per_structure')
+        ]
